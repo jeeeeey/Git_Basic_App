24e5c96ce6867da81ccec88bd3405bfaa1e1abf8
wjeeeeey <wjeeeeey@gmail.com>
2023-12-01 15:17:55 +0900

    index.js add 함수 변경과 substract 함수 추가

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 /* 
     index.html 을 위한 js 파일 
 */
-function add(a, b) {
-    return a + b; 
+function add(a, b, c) {
+    if(c !== undefined) {
+        return a + b; 
+    }
+}
+
+function substract(a, b) {
+    if(a > b) {
+        return a - b;
+    } else {
+        return b - a;
+    }
 }
